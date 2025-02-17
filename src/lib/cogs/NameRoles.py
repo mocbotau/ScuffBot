b0ad71dbfd2c4279d6e8a358a73edbfa88370889
MasterOfCubesAU <masterofcubesau@gmail.com>
2025-02-17 17:40:39 +1100

    fix: name role now updates if displayed rank is incorrect

--- src/lib/cogs/NameRoles.py
+++ src/lib/cogs/NameRoles.py
@@ -4,6 +4,7 @@ import logging
 from src.lib.bot import config
 import asyncio
 
+
 class NameRoles(commands.Cog):
 
     def __init__(self, bot):
@@ -18,7 +19,8 @@ class NameRoles(commands.Cog):
     @commands.Cog.listener()
     async def on_member_update(self, before: discord.Member, after: discord.Member):
         if before.roles != after.roles:
-            added_roles = [role for role in after.roles if role not in before.roles and role.id in self.roles]
+            added_roles = [
+                role for role in after.roles if role not in before.roles and role.id in self.roles]
             if added_roles:
                 name = after.display_name.split(" | ")[0]
                 rank = self.roles[added_roles[0].id]
@@ -27,21 +29,26 @@ class NameRoles(commands.Cog):
     async def rank_integrity_check(self):
         await self.bot.wait_until_ready()
         for guild in self.bot.guilds:
-            total_members = len(guild.members)
-            for count, member in enumerate(guild.members):
-                tokens = [token.strip() for token in member.display_name.split("|")]
-                role_intersection = set(config["NAME_ROLES"].keys()).intersection(set([role.id for role in member.roles]))
-                if not role_intersection or tokens[-1] in config["NAME_ROLES"].values():
+            for member in guild.members:
+                old_name = member.display_name
+                tokens = [token.strip()
+                          for token in member.display_name.split("|")]
+                role_intersection = [
+                    role_id for role_id in config["NAME_ROLES"].keys() if role_id in [role.id for role in member.roles]]
+                if not role_intersection or config["NAME_ROLES"][role_intersection[0]] == tokens[-1]:
                     continue
                 for role_id in config["NAME_ROLES"]:
                     if role_id in role_intersection:
                         try:
                             await member.edit(nick=f"{tokens[0]} | {config['NAME_ROLES'][role_id]}", reason="Name does not match RL role.")
                         except Exception as e:
-                            self.logger.info(f"FAILED ({e}): {member.display_name} => {tokens[0]} | {config['NAME_ROLES'][role_id]}")
+                            self.logger.info(
+                                f"FAILED ({e}): {old_name} => {tokens[0]} | {config['NAME_ROLES'][role_id]}")
                         else:
-                            self.logger.info(f"{member.display_name} => {tokens[0]} | {config['NAME_ROLES'][role_id]}")
+                            self.logger.info(
+                                f"{old_name} => {tokens[0]} | {config['NAME_ROLES'][role_id]}")
                         break
+
 
 async def setup(bot):
     await bot.add_cog(NameRoles(bot))
